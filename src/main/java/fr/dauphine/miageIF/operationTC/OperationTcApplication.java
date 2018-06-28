4c185292083b8136d5d62f221cf1f4e3aee262ff
rasolohery tianome <lifakpo@gmail.com>
2018-06-28 15:25:21 +0200

    ajout src

---
+++ src/main/java/fr/dauphine/miageIF/operationTC/OperationTcApplication.java
@@ -0,0 +1,12 @@
+package fr.dauphine.miageIF.operationTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationTcApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/dauphine/miageIF/operationTC/OperationTcApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.dauphine.miageIF.operationTC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OperationTcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
